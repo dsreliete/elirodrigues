de7319a1851bf752dcde2e611736f177a8aabd8f
dsreliete <dsreliete@gmail.com>
2019-11-06 16:10:33 -0500

    updated card deck content

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,19 @@ $().ready(function (){
         window.open("https://github.com/dsreliete/WeatherApp");
     })
 
-    $("fiveThingsApp").click(function() {
+    $("#fiveThingsApp").click(function() {
         window.open("https://github.com/dsreliete/Five-Things-Android-App");
+    })
+
+    $("#pollsApp").click(function(){
+        window.open("https://github.com/dsreliete/Polls");
+    })
+
+    $("#heroesApi").click(function(){
+        window.open("https://github.com/dsreliete/Nucamp");
+    })
+
+    $("#blogApp").click(function(){
+        window.open("https://github.com/dsreliete/Blog");
     })
 })
